4f840ebd314fab43875c72290a3a1eaf255bbd97
ormin05 <164223133+ormin05@users.noreply.github.com>
2024-03-20 10:12:34 -0700

    Add files via upload

---
+++ 2D_Game.py
@@ -0,0 +1,27 @@
+x = [[1,2,3],
+     [4,5,6,8],
+     [7]]
+print(x[0][0])
+print(x[1][1])
+print(x[0])
+print(x[2][0])
+print(x[1][2])
+x[1][2] = 4
+print(x)
+x[0][0] = 100
+print(x)
+
+
+
+
+'''num = [1,2,2,3,3,3,4]
+for  i in range(len(num)):
+    if num[i]%2 == 0:
+        print(num[i], end =" ")
+    else:
+        num[i] = num[i] + 1
+print(end='\n')
+print(num)
+for i in num:
+    count += i
+print (count)'''
